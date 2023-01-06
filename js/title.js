03d572fc4e2def5ad68172b4f7a7711d57d50507
https-White <121564445+https-White@users.noreply.github.com>
2023-01-07 03:32:01 +0700

    Add files via upload

---
+++ js/title.js
@@ -0,0 +1,35 @@
+﻿if(document.addEventListener){
+	document.addEventListener("DOMContentLoaded", function(){
+		loaded();
+	});
+} else if(document.attachEvent){
+	document.attachEvent("onreadystatechange", function(){
+		loaded();
+	});
+}
+
+function loaded(){
+	
+	setInterval(loop, 300);
+
+}
+
+var x = 0;
+
+var titleText = [ "|",
+					"M|",
+					"M |",
+					"M i|",
+					"M i |",
+					"M i p|",
+					"M i p |",
+					"M i p e|",
+					"M i p e |",
+					"M i p e d|",
+					"M i p e d |",	];
+
+function loop(){
+
+	document.getElementsByTagName("title")[0].innerHTML = titleText[x++%titleText.length];
+
+}
